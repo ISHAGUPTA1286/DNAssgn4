4eac3e222f084fecd0a0f923557832afc88ffca7
ace-spadez <anvay200.1@gmail.com>
2020-10-06 14:55:25 +0530

    Added major functions

--- Dls.py
+++ Dls.py
@@ -2,29 +2,32 @@ import subprocess as sp
 import pymysql
 import pymysql.cursors
 from prettytable import PrettyTable
+import datetime
+
 
 def Exists(model_name, field_name, field_value):
     checkuserquery = "SELECT COUNT(%s.%s) FROM %s WHERE %s.%s='%d';" % (
         model_name, field_name, model_name, model_name, field_name, field_value)
     cur.execute(checkuserquery)
-    count = cur.fetchall()[0]["COUNT(%s.%s)"%(model_name,field_name)]
-    if(count==0):
+    count = cur.fetchall()[0]["COUNT(%s.%s)" % (model_name, field_name)]
+    if(count == 0):
         return False
     return True
 
+
 def printPretty(dict_list):
-    if len(dict_list)==0:
-        print("No items in the database")
+    if len(dict_list) == 0:
+        print("No item returned by the database.\nThis could be due to no matching input.")
         return
     x = PrettyTable()
-    field_names =[]
+    field_names = []
     for val in dict_list[0]:
-        field_names+=[val]
+        field_names += [val]
     x.field_names = field_names
     for val in dict_list:
-        y=[]
+        y = []
         for val2 in val:
-            y+=[val[val2]]
+            y += [val[val2]]
         x.add_row(y)
     print(x)
 
@@ -73,22 +76,6 @@ def addStation():
         print(">>>>>>>>>>>>>", e)
 
 
-# def addBee():
-#     pass
-
-
-# def addBeehive():
-#     pass
-
-
-# def addBeehiveHole():
-#     pass
-
-
-# def addContainer():
-#     pass
-
-
 def showAllUsers():
     try:
         query = "SELECT * FROM `USER`"
@@ -101,14 +88,13 @@ def showAllUsers():
         print(">>>>>>>>>>>>>", e)
 
 
-
 def showAllBees():
     try:
         query = "SELECT * FROM `BEE`"
         cur.execute(query)
         result = cur.fetchall()
         printPretty(result)
-    
+
     except Exception as e:
         con.rollback()
         print("bee data is private to company")
@@ -117,16 +103,15 @@ def showAllBees():
 
 def showAvalableBees():
     try:
-        query = "SELECT * FROM `DOCKED_BEE`"
+        query = "SELECT * FROM `BEE` where BEE.bee_id not in (SELECT TRANSIT.bee_id from TRANSIT)"
         cur.execute(query)
         result = cur.fetchall()
         printPretty(result)
-    
+
     except Exception as e:
         con.rollback()
         print("bee data is private to company")
         print(">>>>>>>>>>>>>", e)
-    
 
 
 def showAllBeehives():
@@ -135,7 +120,7 @@ def showAllBeehives():
         cur.execute(query)
         result = cur.fetchall()
         printPretty(result)
-    
+
     except Exception as e:
         con.rollback()
         print("beehive data is private to company")
@@ -148,7 +133,7 @@ def showAllContainers():
         cur.execute(query)
         result = cur.fetchall()
         printPretty(result)
-    
+
     except Exception as e:
         con.rollback()
         print("CONTAINER data is private to company")
@@ -156,11 +141,69 @@ def showAllContainers():
 
 
 def showAvailableContaniers():
+    try:
+        query = "SELECT CONTAINER.container_id, CONTAINER.weight from CONTAINER WHERE CONTAINER.container_id not in (SELECT DOCKED.container_id from DOCKED) and CONTAINER.container_id not in (SELECT TRANSIT.container_id from TRANSIT);"
+        cur.execute(query)
+        result = cur.fetchall()
+        printPretty(result)
+
+    except Exception as e:
+        con.rollback()
+        print("CONTAINER data is private to company")
+        print(">>>>>>>>>>>>>", e)
+
+
+def fetchOneAvailableBee():
     pass
+    query = "SELECT * FROM `BEE` where BEE.bee_id not in (SELECT TRANSIT.bee_id from TRANSIT)"
+    # query = "SELECT TOP 1"
+    cur.execute(query)
+    result = cur.fetchall()
+    if(len(result) == 0):
+        return -1
+    else:
+        return result[0]['bee_id']
 
 
 def sendCourier():
-    pass
+    try:
+        row = {}
+        print("please enter delivery details")
+        row['sender_id'] = int(input("Sender ID: "))
+        row['receiver_id'] = int(input("Reciever ID: "))
+        row['container_id'] = int(input("Container ID: "))
+
+        # do validation for availibility of the container
+        #
+
+        # make delivry model
+        query = "INSERT INTO DELIVERY VALUES(%d,%d,%d)" % (
+            row['sender_id'], row['receiver_id'], row['container_id'])
+        cur.execute(query)
+        con.commit()
+
+        # make delivery status model
+        dt = str(datetime.datetime.now())
+        query = "INSERT INTO DELIVERY_STATUS VALUES('%s','%s')" % (
+            row['container_id'], dt)
+        cur.execute(query)
+        con.commit()
+
+        # make delivery status=transit
+        bee_id = fetchOneAvailableBee()
+        if(bee_id == -1):
+            print("No bee available to allocate. Sorry for your inconviniance")
+            return
+        query = "INSERT INTO TRANSIT VALUES(%d,%d)" % (
+            row['container_id'], bee_id)
+        cur.execute(query)
+        con.commit()
+        print('Delivery started')
+
+    except Exception as e:
+        con.rollback()
+        print("Unable to add delivery")
+        print(">>>>>>>>>>>>>", e)
 
 
 def showAllDeliveries():
@@ -169,78 +212,229 @@ def showAllDeliveries():
         cur.execute(query)
         result = cur.fetchall()
         printPretty(result)
-    
+
     except Exception as e:
         con.rollback()
-        print("dilivery data is private to company")
+        print("delivery data is private to company")
         print(">>>>>>>>>>>>>", e)
 
+
 def showDeliveryStatus():
-    pass
+    try:
+        container_id = int(input("Enter the container id: "))
+        query = "SELECT * from DELIVERY_STATUS where container_id=%d" % container_id
+        cur.execute(query)
+        result = cur.fetchall()
+        printPretty(result)
+
+        query = "SELECT * from DOCKED where container_id=%d"%container_id
+        cur.execute(query)
+        result = cur.fetchall()
+        if len(result)==0:
+            pass
+        else:
+            print("THe container is docked in beehive with id %d"%result[0]['hive_id'])
+
+
+        query = "SELECT * from TRANSIT where container_id=%d"%container_id
+        cur.execute(query)
+        result = cur.fetchall()
+        if len(result)==0:
+            pass
+        else:
+            print("THe container is in transit by bee with id %d"%result[0]['bee_id'])
+
+    except Exception as e:
+        con.rollback()
+        print("ERROr")
+        print(">>>>>>>>>>>>>", e)
 
 
 def deleteUserWithID():
-    pass
+    try:
+        user_id = int(input("User ID: "))
+        query1 = "DELETE from SUBSCRIPTION where user_id=%d"%user_id
+        query2 = "DELETE from STATION where user_id=%d"%user_id
+        query3 = "DELETE from USER where user_id=%d" % user_id
+
+        
+        cur.execute(query1)
+        con.commit()
+        cur.execute(query2)
+        con.commit()
+        cur.execute(query3)
+        con.commit()
+
+        print("Successfully deleted the user")
+    except Exception as e:
+        con.rollback()
+        print("CANNOT delete user because there might be pending deliveries or:")
+        print(">>>>>>>>>>>>>", e)
+
 
 def updateUserLocation():
-    # take userid, lat, long as input and update the user location
-    pass
+    try:
+        user_id = int(input("User Id: "))
+        latitude = float(input("Latitude: "))
+        longitude = float(input("Longitude: "))
+        query = "UPDATE USER SET latitude=%f, longitude=%f where user_id=%d" % (
+            latitude, longitude, user_id)
+        cur.execute(query)
+        con.commit()
+        print("Successfully updated the user location")
+
+
+    except Exception as e:
+        con.rollback()
+        print("ERROr")
+        print(">>>>>>>>>>>>>", e)
+
 
 def updateStationLocation():
-    # take station_id, lat, longas input
-    pass
+    try:
+        station_id = int(input("STATION Id: "))
+        latitude = float(input("Latitude: "))
+        longitude = float(input("Longitude: "))
+        query = "UPDATE STATION SET latitude=%f, longitude=%f where station_id=%d" % (
+            latitude, longitude, station_id)
+        cur.execute(query)
+        con.commit()
+        print("Successfully updated the station location")
+
+    except Exception as e:
+        con.rollback()
+        print("ERROr")
+        print(">>>>>>>>>>>>>", e)
+
 
 def completeDelivery():
-    #delete delivery, delivery_status, and container things
-    pass
+    # delete delivery, delivery_status, and container things
+    try:
+        container_id = int(input("Enter Container ID: "))
+        query4 = "DELETE from DELIVERY where container_id=%d" % container_id
+        query3 = "DELETE from DELIVERY_STATUS where container_id=%d" % container_id
+        query1 = "DELETE from DOCKED where container_id=%d" % container_id
+        query2 = "DELETE from TRANSIT where container_id=%d" % container_id
+        cur.execute(query1)
+        con.commit()        
+        cur.execute(query2)
+        con.commit()        
+        cur.execute(query3)
+        con.commit()        
+        cur.execute(query4)
+        con.commit()
 
-def dockTheBeeWithID():
-    #take bee, and dock it, change delivery status to docked, change bee status to docked, delete transit status and transit bee
-    pass
+        print("Delivery completed")
+    except Exception as e:
+        con.rollback()
+        print("ERROr")
+        print(">>>>>>>>>>>>>", e)    
 
-def undockTheBeeWithID():
-    pass
-    # do the opposite of docking the bee
+
+def dockTheContainerWithIDToBeeHiveWithId():
+    try:
+        container_id = int(input("Container ID: "))
+        hive_id = int(input("Hive ID: "))
+
+        #validate if container is in transit
+        query = "SELECT * from TRANSIT where container_id=%d"%container_id
+        cur.execute(query)
+        if(len(cur.fetchall())==0):
+            raise("The container ID is not valid")
+        query = "DELETE from TRANSIT where container_id=%d"%container_id
+        cur.execute(query)
+        con.commit()
+
+        query = "INSERT INTO DOCKED VALUES(%d,%d)"%(container_id,hive_id)
+        cur.execute(query)
+        con.commit()
+        print("Succesfully docked the container")
+    except Exception as e:
+        con.rollback()
+        print("ERROr")
+        print(">>>>>>>>>>>>>", e)  
+
+
+def undockTheContainerWithID():
+    try:
+        container_id = int(input("Container ID: "))
+        # hive_id = int(input("Hive ID: "))
+
+        #validate if container is docked
+        query = "SELECT * from DOCKED where container_id=%d"%container_id
+        cur.execute(query)
+        if(len(cur.fetchall())==0):
+            raise("The container ID is not valid")
+        query = "DELETE from DOCKED where container_id=%d"%container_id
+        cur.execute(query)
+        con.commit()
+
+        bee_id = fetchOneAvailableBee()
+        if(bee_id==-1):
+            raise("No available BEE")
+        query = "INSERT INTO TRANSIT VALUES(%d,%d)"%(container_id,bee_id)
+        cur.execute(query)
+        con.commit()
+
+        print("Succesfully undocked the container")
+
+
+    except Exception as e:
+        con.rollback()
+        print("ERROr")
+        print(">>>>>>>>>>>>>", e)  
+
+
 
 def addUserSubscription():
-    #take the userid and wallet amount and add subscription
+    # take the userid and wallet amount and add subscription
     pass
+
 
 def updateSubscription():
-    #take the subscription_id and added amount and update the wallet
+    # take the subscription_id and added amount and update the wallet
     pass
 
+
 def showUserSubscriptions():
-    #show all the user subsriptions for the given user_id
+    # show all the user subsriptions for the given user_id
     pass
+
 
 def findBees():
     pass
-    #take lat and long and radius and find the bees in the given radius of that point
+    # take lat and long and radius and find the bees in the given radius of that point
+
 
 def updateBeeLocation():
     pass
-    #take the given bee_id, lat and long and update
+    # take the given bee_id, lat and long and update
+
 
 def findHives():
-    #take the capacity as parameter and filter all the hives with capacity above given
+    # take the capacity as parameter and filter all the hives with capacity above given
     pass
+
 
 def findCont():
-    #take weight as input and find all the containers with weight aboove the given weight
+    # take weight as input and find all the containers with weight aboove the given weight
     pass
+
 
 def findTimePassed():
-    #take delivery id as input and find the time passed from the bg_time.
+    # take delivery id as input and find the time passed from the bg_time.
     pass
+
 
 def searchUserWithFirstName():
-    #take firstname as input and give info about the user
+    # take firstname as input and give info about the user
     pass
 
+
 def searchDeliveriesUserIsSending():
-    #give all the deliveries pending for the user as sender
+    # give all the deliveries pending for the user as sender
     pass
+
 
 def searchAllDeliveriesUserIsReceiving():
     pass
@@ -260,29 +454,32 @@ def dispatch(ch):
     elif(ch == 4):
         showAllBees()
     elif(ch == 5):
-        addStation()
+        showAvailableContaniers()
     elif(ch == 6):
-        showAllUsers()
+        showAllContainers()
     elif(ch == 7):
-        showAllBees()
+        showAvalableBees()
     elif(ch == 8):
-        addStation()
+        showAllBeehives()
     elif(ch == 9):
-        showAllUsers()
+        showAllDeliveries()
     elif(ch == 10):
-        showAllBees()
+        sendCourier()
     elif(ch == 11):
-        addStation()
+        updateUserLocation()
     elif(ch == 12):
-        showAllUsers()
+        updateStationLocation()
     elif(ch == 13):
-        showAllBees()
+        completeDelivery()
     elif(ch == 14):
-        addStation()
+        dockTheContainerWithIDToBeeHiveWithId()
     elif(ch == 15):
-        showAllUsers()
+        undockTheContainerWithID()
     elif(ch == 16):
-        showAllBees()
+        showDeliveryStatus()
+    elif(ch == 17):
+        deleteUserWithID()
+    
 
     else:
         print("Error: Invalid Option")
@@ -321,10 +518,23 @@ while(1):
                 print("2. Add Station")  # Fire an Employee
                 print("3. Show All Users")  # Promote Employee
                 print("4. Show All Bees")  # Employee Statistics
-                print("5. Logout")
+                print("5. show available containers")
+                print("6. show all containers")
+                print("7. show available bees")
+                print("8. show all beehives")
+                print("9. show all Deliveries")
+                print("10. Send courier")
+                print("11. update user location")
+                print("12. update station location")
+                print("13. complete delivery")
+                print("14. dock The Container With ID To Bee Hive With Id")
+                print("15. undock The Container With ID ")
+                print("16. Show delivery status ")
+                print("17. Delete User ")
+                # print("9. show all Deliveries")
                 ch = int(input("Enter choice> "))
                 tmp = sp.call('clear', shell=True)
-                if ch == 5:
+                if ch == 25:
                     break
                 else:
                     dispatch(ch)
@@ -334,29 +544,3 @@ while(1):
         tmp = sp.call('clear', shell=True)
         print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
         tmp = input("Enter any key to CONTINUE>")
-
-# connection = pymysql.connect(host='localhost',
-#                              user='user',
-#                              password='passwd',
-#                              db='db',
-#                              charset='utf8mb4',
-#                              cursorclass=pymysql.cursors.DictCursor)
-
-# try:
-#     with connection.cursor() as cursor:
-#         # Create a new record
-#         sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-#         cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-
-#     # connection is not autocommit by default. So you must commit to save
-#     # your changes.
-#     connection.commit()
-
-#     with connection.cursor() as cursor:
-#         # Read a single record
-#         sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-#         cursor.execute(sql, ('webmaster@python.org',))
-#         result = cursor.fetchone()
-#         print(result)
-# finally:
-#     connection.close()
